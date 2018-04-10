aa125bf3a29f995bf2a5fd5d290c9b0eee64bcf0
wenyifeng1123 <33321977+wenyifeng1123@users.noreply.github.com>
2018-04-10 16:31:18 +0200

    Add files via upload

---
+++ visualize/integratAndMerge/Ui_matplotlib_pyqt_2List2.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'Ui_matplotlib_pyqt_2List2.ui'
+#
+# Created by: PyQt5 UI code generator 5.6
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1088, 800)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.matplotlibwidget_static = MatplotlibWidget(self.centralwidget)
+        self.matplotlibwidget_static.setGeometry(QtCore.QRect(399, 10, 301, 731))
+        self.matplotlibwidget_static.setObjectName("matplotlibwidget_static")
+        self.matplotlibwidget_static_2 = MatplotlibWidget(self.centralwidget)
+        self.matplotlibwidget_static_2.setGeometry(QtCore.QRect(230, 30, 821, 661))
+        self.matplotlibwidget_static_2.setObjectName("matplotlibwidget_static_2")
+        self.horizontalSlider = QtWidgets.QSlider(self.matplotlibwidget_static_2)
+        self.horizontalSlider.setGeometry(QtCore.QRect(20, 630, 531, 22))
+        self.horizontalSlider.setOrientation(QtCore.Qt.Horizontal)
+        self.horizontalSlider.setObjectName("horizontalSlider")
+        self.lcdNumber = QtWidgets.QLCDNumber(self.matplotlibwidget_static_2)
+        self.lcdNumber.setGeometry(QtCore.QRect(570, 630, 71, 23))
+        self.lcdNumber.setObjectName("lcdNumber")
+        self.groupBox = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBox.setGeometry(QtCore.QRect(10, 200, 191, 51))
+        self.groupBox.setObjectName("groupBox")
+        self.wy2 = QtWidgets.QPushButton(self.groupBox)
+        self.wy2.setGeometry(QtCore.QRect(20, 20, 151, 21))
+        self.wy2.setObjectName("wy2")
+        self.groupBox_2 = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBox_2.setGeometry(QtCore.QRect(10, 260, 191, 211))
+        self.groupBox_2.setObjectName("groupBox_2")
+        self.wy3 = QtWidgets.QPushButton(self.groupBox_2)
+        self.wy3.setGeometry(QtCore.QRect(20, 20, 151, 23))
+        self.wy3.setObjectName("wy3")
+        self.listView = QtWidgets.QListView(self.groupBox_2)
+        self.listView.setGeometry(QtCore.QRect(20, 50, 151, 121))
+        self.listView.setObjectName("listView")
+        self.wy4 = QtWidgets.QPushButton(self.groupBox_2)
+        self.wy4.setGeometry(QtCore.QRect(20, 180, 151, 23))
+        self.wy4.setObjectName("wy4")
+        self.groupBox_3 = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBox_3.setGeometry(QtCore.QRect(10, 30, 191, 161))
+        self.groupBox_3.setObjectName("groupBox_3")
+        self.wy1 = QtWidgets.QPushButton(self.groupBox_3)
+        self.wy1.setGeometry(QtCore.QRect(20, 20, 151, 23))
+        self.wy1.setObjectName("wy1")
+        self.groupBox_4 = QtWidgets.QGroupBox(self.groupBox_3)
+        self.groupBox_4.setGeometry(QtCore.QRect(20, 60, 151, 91))
+        self.groupBox_4.setObjectName("groupBox_4")
+        self.textEdit = QtWidgets.QTextEdit(self.groupBox_4)
+        self.textEdit.setGeometry(QtCore.QRect(0, 20, 151, 71))
+        self.textEdit.setObjectName("textEdit")
+        self.groupBox_5 = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBox_5.setGeometry(QtCore.QRect(10, 480, 191, 51))
+        self.groupBox_5.setObjectName("groupBox_5")
+        self.wy5 = QtWidgets.QPushButton(self.groupBox_5)
+        self.wy5.setGeometry(QtCore.QRect(20, 20, 151, 23))
+        self.wy5.setObjectName("wy5")
+        self.matplotlibwidget_static_3 = MatplotlibWidget(self.centralwidget)
+        self.matplotlibwidget_static_3.setGeometry(QtCore.QRect(270, 20, 741, 571))
+        self.matplotlibwidget_static_3.setObjectName("matplotlibwidget_static_3")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1088, 23))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.groupBox.setTitle(_translate("MainWindow", "Visualization of Weights"))
+        self.wy2.setText(_translate("MainWindow", "Show the Weights"))
+        self.groupBox_2.setTitle(_translate("MainWindow", "Visualization of Architecture"))
+        self.wy3.setText(_translate("MainWindow", "Show the Layers\' Names"))
+        self.wy4.setText(_translate("MainWindow", "Show the Plot"))
+        self.groupBox_3.setTitle(_translate("MainWindow", "Choose one H5 file"))
+        self.wy1.setText(_translate("MainWindow", "Choose the file"))
+        self.groupBox_4.setTitle(_translate("MainWindow", "The chosen file"))
+        self.groupBox_5.setTitle(_translate("MainWindow", "DeepVisualization"))
+        self.wy5.setText(_translate("MainWindow", "Show Subset Selection"))
+
+from matplotlibwidget import MatplotlibWidget
